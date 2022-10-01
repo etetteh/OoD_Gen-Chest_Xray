d9bf050a22e2db17b0639fa9c92b12fa3f20aa21
etetteh <cutetetteh@gmail.com>
2022-10-01 08:14:58 -0400

    Improve code modularity

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import os
 import csv
+import torchxrayvision as xrv
+import torch, torchvision, torchvision.transforms
+
 
 def write_results(filename: str, field_names: list, results: dict):
     read_file = open(filename, "r")
@@ -8,7 +11,7 @@ def write_results(filename: str, field_names: list, results: dict):
     new = []
     row = {}
     for r in results_file:
-        if r['Model'] == results['Model']:
+        if r["Model"] == results["Model"]:
             for key, value in results.items():
                 row[key] = value
             update = row
@@ -34,4 +37,198 @@ def write_results(filename: str, field_names: list, results: dict):
         data.writerows([new])
     
     update_file.close()
+
+
+def load_data(cfg):
+    data_aug = torchvision.transforms.Compose([
+            xrv.datasets.ToPILImage(),
+            torchvision.transforms.RandomAffine(
+                cfg.data_aug_rot, 
+                translate=(cfg.data_aug_trans, cfg.data_aug_trans), 
+                scale=(1.0-cfg.data_aug_scale, 1.0+cfg.data_aug_scale)
+            ),                                    
+            torchvision.transforms.ToTensor()
+            ])
+
+    transforms = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(), xrv.datasets.XRayResizer(cfg.data_resize)])
+
+    ### Load NIH Dataset ### 
+    NIH_dataset = None
+    if "nih" == cfg.val_data or "nih" in cfg.train_datas:
+        NIH_dataset = xrv.datasets.NIH_Dataset(
+                imgpath=cfg.dataset_dir + "/images-224-NIH", 
+                csvpath=cfg.dataset_dir + "/Data_Entry_2017_v2020.csv.gz",
+                bbox_list_path=cfg.dataset_dir + "/BBox_List_2017.csv.gz",
+                transform=transforms, data_aug=data_aug, unique_patients=False)
+        xrv.datasets.relabel_dataset(cfg.pathologies, NIH_dataset)
+
+    ## Load CHEXPERT Dataset ###
+    CHEX_dataset = None
+    if "cx" == cfg.val_data or "cx" in cfg.train_datas:
+        CHEX_dataset = xrv.datasets.CheX_Dataset(
+                imgpath=cfg.dataset_dir + "/CheXpert-v1.0-small",
+                csvpath=cfg.dataset_dir + "/CheXpert-v1.0-small/train.csv",
+                transform=transforms, data_aug=data_aug, unique_patients=False)
+        xrv.datasets.relabel_dataset(cfg.pathologies, CHEX_dataset)
+
+    # ### Load MIMIC_CH Dataset ###
+    MIMIC_CH_dataset = None
+    if "mc" == cfg.val_data or "mc" in cfg.train_datas:
+        MIMIC_CH_dataset = xrv.datasets.MIMIC_Dataset(
+            imgpath=cfg.dataset_dir + "/images-224-MIMIC/files",
+            csvpath=cfg.dataset_dir + "/MIMICCXR-2.0/mimic-cxr-2.0.0-chexpert.csv.gz",
+            metacsvpath=cfg.dataset_dir + "/MIMICCXR-2.0/mimic-cxr-2.0.0-metadata.csv.gz",
+            transform=transforms, data_aug=data_aug, unique_patients=False)
+        xrv.datasets.relabel_dataset(cfg.pathologies, MIMIC_CH_dataset)
+
+    ### Load PADCHEST Dataset ###
+    PC_dataset = None
+    if "pc" == cfg.val_data or "pc" in cfg.train_datas:
+        PC_dataset = xrv.datasets.PC_Dataset(
+                imgpath=cfg.dataset_dir + "/PC/images-224",
+                csvpath=cfg.dataset_dir + "/PC/PADCHEST_chest_x_ray_images_labels_160K_01.02.19.csv",
+                transform=transforms, data_aug=data_aug, unique_patients=False)
+        xrv.datasets.relabel_dataset(cfg.pathologies, PC_dataset)
+
+
+    print(f"\nCommon pathologies among all train and validation datasets: {cfg.pathologies}")
+
+
+    datasets = {
+        "nih": NIH_dataset,
+        "cx": CHEX_dataset,
+        "mc": MIMIC_CH_dataset,
+        "pc": PC_dataset,
+    }
+
+    return datasets
+
+
+def load_inference_data(cfg):
+    transforms = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(), xrv.datasets.XRayResizer(cfg.data_resize)])
+    
+    if "nih" in cfg.test_data:
+        ### Load NIH Dataset ### 
+        NIH_dataset = xrv.datasets.NIH_Dataset(
+                imgpath=cfg.dataset_dir + "/images-224-NIH", 
+                csvpath=cfg.dataset_dir + "/Data_Entry_2017_v2020.csv.gz",
+                bbox_list_path=cfg.dataset_dir + "/BBox_List_2017.csv.gz",
+                transform=transforms, data_aug=None, unique_patients=False)
+        xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, NIH_dataset)
+        test_data = NIH_dataset
+
+    if "mc" in cfg.test_data:
+        # ### Load MIMIC_CH Dataset ###
+        MIMIC_CH_dataset = xrv.datasets.MIMIC_Dataset(
+            imgpath=cfg.dataset_dir + "/images-224-MIMIC/files",
+            csvpath=cfg.dataset_dir + "/MIMICCXR-2.0/mimic-cxr-2.0.0-chexpert.csv.gz",
+            metacsvpath=cfg.dataset_dir + "/MIMICCXR-2.0/mimic-cxr-2.0.0-metadata.csv.gz",
+            transform=transforms, data_aug=None, unique_patients=False)
+        xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, MIMIC_CH_dataset)
+        test_data = MIMIC_CH_dataset 
+
+    if "cx" in cfg.test_data:
+        ## Load CHEXPERT Dataset ###
+        CHEX_dataset = xrv.datasets.CheX_Dataset(
+                imgpath=cfg.dataset_dir + "/CheXpert-v1.0-small",
+                csvpath=cfg.dataset_dir + "/CheXpert-v1.0-small/train.csv",
+                transform=transforms, data_aug=None, unique_patients=False)
+        xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, CHEX_dataset)
+        test_data = CHEX_dataset
+
+    if "pc" in cfg.test_data:
+        ### Load PADCHEST Dataset ###
+        PC_dataset = xrv.datasets.PC_Dataset(
+                imgpath=cfg.dataset_dir + "/PC/images-224",
+                csvpath=cfg.dataset_dir + "/PC/PADCHEST_chest_x_ray_images_labels_160K_01.02.19.csv",
+                transform=transforms, data_aug=None, unique_patients=False)
+        xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, PC_dataset)
+        test_data = PC_dataset
+
+    if "gg" in cfg.test_data:
+        ### Load GOOGLE Dataset ###
+        GOOGLE_dataset = xrv.datasets.NIH_Google_Dataset(
+                imgpath=cfg.dataset_dir + "/images-224-NIH",
+                csvpath=cfg.dataset_dir + "/google2019_nih-chest-xray-labels.csv.gz",
+                transform=transforms, data_aug=None
+                )
+        xrv.datasets.default_pathologies = ['Pneumothorax', 'Fracture']
+        xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, GOOGLE_dataset)
+        test_data = GOOGLE_dataset
+
+    if "op" in cfg.test_data:
+        ### Load OPENI Dataset ###
+        OPENI_dataset = xrv.datasets.Openi_Dataset(
+                imgpath=cfg.dataset_dir + "/images-openi/",
+                xmlpath=cfg.dataset_dir + "/NLMCXR_reports.tgz", 
+                dicomcsv_path=cfg.dataset_dir + "/nlmcxr_dicom_metadata.csv.gz",
+                tsnepacsv_path=cfg.dataset_dir + "/nlmcxr_tsne_pa.csv.gz",
+                transform=transforms, data_aug=None
+                )
+        xrv.datasets.default_pathologies = ['Effusion', 'Cardiomegaly', 'Edema']
+        xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, OPENI_dataset)
+        test_data = OPENI_dataset
+
+    if "rs" in cfg.test_data:    
+        ### Load RSNA Dataset ###
+        RSNA_dataset = xrv.datasets.RSNA_Pneumonia_Dataset(
+            imgpath=cfg.dataset_dir + "/kaggle-pneumonia-jpg/stage_2_train_images_jpg",
+            csvpath=cfg.dataset_dir + "/kaggle-pneumonia-jpg/stage_2_train_labels.csv",
+            dicomcsvpath=cfg.dataset_dir + "/kaggle_stage_2_train_images_dicom_headers.csv.gz",
+            transform=transforms, data_aug=None, unique_patients=False
+            )
+        xrv.datasets.default_pathologies = ['Lung Opacity', 'Pneumonia']
+        xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, RSNA_dataset)
+        test_data = RSNA_dataset
+    
+    return test_data
+
+
+def create_q_model(cfg, model):
+    num_ftrs = model.fc.in_features
+    model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+    model_features = torch.nn.Sequential(
+                        model.quant,  
+                        model.conv1,
+                        model.bn1,
+                        model.relu,
+                        model.maxpool,
+                        model.layer1,
+                        model.layer2,
+                        model.layer3,
+                        model.layer4,
+                        model.avgpool,
+                        model.dequant,  
+                      )
+
+    new_head = torch.nn.Sequential(
+                    torch.nn.Dropout(p=cfg.dropout),
+                    torch.nn.Linear(num_ftrs, cfg.num_labels)
+               )
+
+    new_model = torch.nn.Sequential(
+                    model_features,
+                    torch.nn.Flatten(1),
+                    new_head,
+                )
+    return new_model
+
+def create_model(cfg, model):
+    if "resnet" in str(model.__class__):
+        num_ftrs = model.fc.in_features
+        model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        model.fc = torch.nn.Sequential(
+                    torch.nn.Dropout(p=cfg.dropout),
+                    torch.nn.Linear(num_ftrs, cfg.num_labels)
+               )                       
+        
+    elif "densenet" in str(model.__class__):
+        num_ftrs = model.classifier.in_features
+        model.features.conv0 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        model.classifier = torch.nn.Sequential(
+                    torch.nn.Dropout(p=cfg.dropout),
+                    torch.nn.Linear(num_ftrs, cfg.num_labels)
+               )
+
+    return model
 
--- xrv_test.py
+++ xrv_test.py
@@ -3,15 +3,11 @@ torch.multiprocessing.set_sharing_strategy('file_system')
 
 import csv
 import pickle
-import PIL
-import pprint
 import random
 import argparse
-import os,sys,inspect
+import os
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 from glob import glob
 from os.path import exists, join
@@ -19,39 +15,13 @@ from tqdm import tqdm as tqdm_base
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
-import torchvision, torchvision.transforms
-import skimage.transform
+from torch.utils.data import DataLoader, SequentialSampler
 import sklearn.metrics
-import sklearn, sklearn.model_selection
-from sklearn.metrics import roc_auc_score, accuracy_score
+from sklearn.metrics import roc_auc_score
 
+import utils
 import torchxrayvision as xrv
 
-
-def str2bool(v):
-    return v.lower() in ("yes", "true", "t", "1")
-
-parser = argparse.ArgumentParser(description='X-RAY Pathology Detection')
-parser.add_argument('--seed', type=int, default=0, help='')
-parser.add_argument('--dataset_dir', type=str, default="./data/")
-parser.add_argument('--dataset_name', type=str, default="nih")
-
-### Data loader
-parser.add_argument('--cuda', type=bool, default=True, help='')
-parser.add_argument('--batch_size', type=int, default=64, help='')
-parser.add_argument('--shuffle', type=bool, default=False, help='')
-parser.add_argument('--num_workers', type=int, default=0, help='')
-parser.add_argument('--num_batches', type=int, default=430, help='')
-
-### Data Augmentation                  
-parser.add_argument('--data_aug_rot', type=int, default=45, help='')
-parser.add_argument('--data_aug_trans', type=float, default=0.15, help='')
-parser.add_argument('--data_aug_scale', type=float, default=0.15, help='')
-
-
-cfg = parser.parse_args()
-print(cfg)
 
 def tqdm(*args, **kwargs):
     if hasattr(tqdm_base, '_instances'):
@@ -59,111 +29,9 @@ def tqdm(*args, **kwargs):
             tqdm_base._decr_instances(instance)
     return tqdm_base(*args, **kwargs)
 
-device = 'cuda' if cfg.cuda else 'cpu'
-if not torch.cuda.is_available() and cfg.cuda:
-    device = 'cpu'
-    print("WARNING: cuda was requested but is not available, using cpu instead.")
-print(f'Using device: {device}')
 
-
-transforms = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(), xrv.datasets.XRayResizer(112)])
-
-if "nih" in cfg.dataset_name:
-    ### Load NIH Dataset ### 
-    NIH_dataset = xrv.datasets.NIH_Dataset(
-            imgpath=cfg.dataset_dir + "/images-224-NIH", 
-            csvpath=cfg.dataset_dir + "/Data_Entry_2017_v2020.csv.gz",
-            bbox_list_path=cfg.dataset_dir + "/BBox_List_2017.csv.gz",
-            transform=transforms, data_aug=None, unique_patients=False)
-    xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, NIH_dataset)
-    test_data = NIH_dataset
-
-if "mc" in cfg.dataset_name:
-    # ### Load MIMIC_CH Dataset ###
-    MIMIC_CH_dataset = xrv.datasets.MIMIC_Dataset(
-        imgpath=cfg.dataset_dir + "/images-224-MIMIC/files",
-        csvpath=cfg.dataset_dir + "/MIMICCXR-2.0/mimic-cxr-2.0.0-chexpert.csv.gz",
-        metacsvpath=cfg.dataset_dir + "/MIMICCXR-2.0/mimic-cxr-2.0.0-metadata.csv.gz",
-        transform=transforms, data_aug=None, unique_patients=False)
-    xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, MIMIC_CH_dataset)
-    test_data = MIMIC_CH_dataset 
-
-if "cx" in cfg.dataset_name:
-    ## Load CHEXPERT Dataset ###
-    CHEX_dataset = xrv.datasets.CheX_Dataset(
-            imgpath=cfg.dataset_dir + "/CheXpert-v1.0-small",
-            csvpath=cfg.dataset_dir + "/CheXpert-v1.0-small/train.csv",
-            transform=transforms, data_aug=None, unique_patients=False)
-    xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, CHEX_dataset)
-    test_data = CHEX_dataset
-
-if "pc" in cfg.dataset_name:
-    ### Load PADCHEST Dataset ###
-    PC_dataset = xrv.datasets.PC_Dataset(
-            imgpath=cfg.dataset_dir + "/PC/images-224",
-            csvpath=cfg.dataset_dir + "/PC/PADCHEST_chest_x_ray_images_labels_160K_01.02.19.csv",
-            transform=transforms, data_aug=None, unique_patients=False)
-    xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, PC_dataset)
-    test_data = PC_dataset
-
-if "gg" in cfg.dataset_name:
-    ### Load GOOGLE Dataset ###
-    GOOGLE_dataset = xrv.datasets.NIH_Google_Dataset(
-            imgpath=cfg.dataset_dir + "/images-224-NIH",
-            csvpath=cfg.dataset_dir + "/google2019_nih-chest-xray-labels.csv.gz",
-            transform=transforms, data_aug=None
-            )
-    xrv.datasets.default_pathologies = ['Pneumothorax', 'Fracture']
-    xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, GOOGLE_dataset)
-    test_data = GOOGLE_dataset
-
-if "op" in cfg.dataset_name:
-    ### Load OPENI Dataset ###
-    OPENI_dataset = xrv.datasets.Openi_Dataset(
-            imgpath=cfg.dataset_dir + "/images-openi/",
-            xmlpath=cfg.dataset_dir + "/NLMCXR_reports.tgz", 
-            dicomcsv_path=cfg.dataset_dir + "/nlmcxr_dicom_metadata.csv.gz",
-            tsnepacsv_path=cfg.dataset_dir + "/nlmcxr_tsne_pa.csv.gz",
-            transform=transforms, data_aug=None
-            )
-    xrv.datasets.default_pathologies = ['Effusion', 'Cardiomegaly', 'Edema']
-    xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, OPENI_dataset)
-    test_data = OPENI_dataset
-
-if "rs" in cfg.dataset_name:    
-    ### Load RSNA Dataset ###
-    RSNA_dataset = xrv.datasets.RSNA_Pneumonia_Dataset(
-        imgpath=cfg.dataset_dir + "/kaggle-pneumonia-jpg/stage_2_train_images_jpg",
-        csvpath=cfg.dataset_dir + "/kaggle-pneumonia-jpg/stage_2_train_labels.csv",
-        dicomcsvpath=cfg.dataset_dir + "/kaggle_stage_2_train_images_dicom_headers.csv.gz",
-        transform=transforms, data_aug=None, unique_patients=False
-        )
-    xrv.datasets.default_pathologies = ['Lung Opacity', 'Pneumonia']
-    xrv.datasets.relabel_dataset(xrv.datasets.default_pathologies, RSNA_dataset)
-    test_data = RSNA_dataset
-
-print(f"Common pathologies among all train and validation datasets: {xrv.datasets.default_pathologies}")
-    
-np.random.seed(cfg.seed)
-random.seed(cfg.seed)
-torch.manual_seed(cfg.seed)
-
-if cfg.cuda:
-    torch.cuda.manual_seed_all(cfg.seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    
-test_loader = DataLoader(test_data,
-                       batch_size=cfg.batch_size,
-                       shuffle=False,
-                       num_workers=cfg.num_workers,
-                       pin_memory=True,
-                       drop_last=True)
-
-###################################### Test ######################################
 def inference(name, model, device, data_loader, criterion, limit=None):
     model.eval()
-
     avg_loss = []
     task_outputs={}
     task_targets={}
@@ -174,11 +42,9 @@ def inference(name, model, device, data_loader, criterion, limit=None):
     with torch.no_grad():
         t = tqdm(data_loader)
         for batch_idx, samples in enumerate(t):
-
             if limit and (batch_idx >= limit):
                 print("breaking out")
                 break
-            
             images = samples["img"].to(device)
             targets = samples["lab"].to(device)
 
@@ -193,12 +59,10 @@ def inference(name, model, device, data_loader, criterion, limit=None):
                 task_target = task_target[mask]
                 if len(task_target) > 0:
                     loss += criterion(task_output.double(), task_target.double())
-                
+        
                 task_outputs[task].append(task_output.detach().cpu().numpy())
                 task_targets[task].append(task_target.detach().cpu().numpy())
-
             loss = loss.sum()
-            
             avg_loss.append(loss.detach().cpu().numpy())
             
         for task in range(len(task_targets)):
@@ -212,26 +76,72 @@ def inference(name, model, device, data_loader, criterion, limit=None):
                 task_aucs.append(task_auc)
             else:
                 task_aucs.append(np.nan)
-
     task_aucs = np.asarray(task_aucs)
     auc = np.mean(task_aucs[~np.isnan(task_aucs)])
     print(f'{name} - Avg AUC = {auc:4.4f}')
-
     return auc, np.mean(avg_loss), task_aucs
 
-model = xrv.models.DenseNet(weights="all")
-model = model.to(device)
 
-criterion = torch.nn.BCEWithLogitsLoss()
+def main(cfg):
+    device = torch.device(cfg.device)
+    
+    np.random.seed(cfg.seed)
+    random.seed(cfg.seed)
+    torch.manual_seed(cfg.seed)
 
-test_auc, test_loss, task_aucs = inference(name='Test',
-                                         model=model,
-                                         device=device,
-                                         data_loader=test_loader,
-                                         criterion=criterion,
-                                         limit=cfg.num_batches//2)
+    if cfg.device == "cuda":
+        torch.cuda.manual_seed_all(cfg.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
-print(f"Average AUC for all pathologies {test_auc:4.4f}")
-print(f"Test loss: {test_loss:4.4f}")                                 
-print(f"AUC for each task {[round(x, 4) for x in task_aucs]}")
+    test_data = utils.load_data_inference(cfg)
+    
+    test_loader = DataLoader(test_data,
+                           batch_size=cfg.batch_size,
+                           shuffle=SequentialSampler(test_data),
+                           num_workers=cfg.num_workers,
+                           pin_memory=True,
+                           drop_last=True)
+    
+    model = xrv.models.DenseNet(weights="all")
+    model = model.to(device)
 
+    criterion = torch.nn.BCEWithLogitsLoss()
+
+    test_auc, test_loss, task_aucs = inference(name='Test',
+                                     model=model,
+                                     device=device,
+                                     data_loader=test_loader,
+                                     criterion=criterion,
+                                     limit=cfg.num_batches//2
+                                    )
+
+    print(f"Average AUC for all pathologies {test_auc:4.4f}")
+    print(f"Test loss: {test_loss:4.4f}")                                 
+    print(f"AUC for each task {[round(x, 4) for x in task_aucs]}")
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(description='X-RAY Pathology Detection')
+    parser.add_argument('--seed', type=int, default=0, help='')
+    parser.add_argument('--dataset_dir', type=str, default="./data/")
+    parser.add_argument('--test_data', type=str, default="nih")
+    parser.add_argument('--device', type=str, default="cpu")
+
+    ### Data loader
+    parser.add_argument('--data_resize', type=int, default=112)
+    parser.add_argument('--batch_size', type=int, default=64, help='')
+    parser.add_argument('--num_workers', type=int, default=0, help='')
+    parser.add_argument('--num_batches', type=int, default=430, help='')
+
+    ### Data Augmentation                  
+    parser.add_argument('--data_aug_rot', type=int, default=45, help='')
+    parser.add_argument('--data_aug_trans', type=float, default=0.15, help='')
+    parser.add_argument('--data_aug_scale', type=float, default=0.15, help='')
+                        
+    return parser
+                        
+
+if __name__ == "__main__":
+    cfg = get_args_parser().parse_args()
+    main(cfg)
