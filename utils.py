cc0cf6e78d20c601c65600b99e8a9991c27bbd49
etetteh <cutetetteh@gmail.com>
2022-10-17 09:31:24 -0400

    Added minor refactoring to ensure clarity of some code

--- utils.py
+++ utils.py
@@ -2,34 +2,36 @@ import os
 import csv
 import hashlib
 import torchxrayvision as xrv
-import torch, torchvision, torchvision.transforms
+import torch
+import torchvision
+import torchvision.transforms
 
-    
+
 def get_cache_dir(data_dir, data_name):
     h = hashlib.sha1(data_dir.encode()).hexdigest()
     cache_dir = os.path.join("~", ".cache", "torchxrayvision", "datasets", f"{data_name}", h[:17] + ".pt")
     cache_dir = os.path.expanduser(cache_dir)
-    
+
     return cache_dir
 
 
 def load_data(cfg):
     data_aug = torchvision.transforms.Compose([
-            xrv.datasets.ToPILImage(),
-            torchvision.transforms.RandomAffine(
-                cfg.data_aug_rot, 
-                translate=(cfg.data_aug_trans, cfg.data_aug_trans), 
-                scale=(1.0-cfg.data_aug_scale, 1.0+cfg.data_aug_scale)
-            ),                                    
-            torchvision.transforms.ToTensor()
+        xrv.datasets.ToPILImage(),
+        torchvision.transforms.RandomAffine(
+            cfg.data_aug_rot,
+            translate=(cfg.data_aug_trans, cfg.data_aug_trans),
+            scale=(1.0 - cfg.data_aug_scale, 1.0 + cfg.data_aug_scale)
+        ),
+        torchvision.transforms.ToTensor()
     ])
 
     transforms = torchvision.transforms.Compose([
-        xrv.datasets.XRayCenterCrop(), 
+        xrv.datasets.XRayCenterCrop(),
         xrv.datasets.XRayResizer(cfg.data_resize)
     ])
 
-    ### Load and cache NIH Dataset ### 
+    # Load and cache NIH Dataset ###
     nih_dataset = None
     if "nih" in cfg.train_datas or "nih" in cfg.val_data or "nih" in cfg.test_data:
         imgdir = os.path.join(cfg.dataset_dir, "images-224-NIH")
@@ -38,19 +40,19 @@ def load_data(cfg):
             nih_dataset, _ = torch.load(cache_dir)
         else:
             nih_dataset = xrv.datasets.NIH_Dataset(
-                    imgpath=imgdir,
-                    csvpath=os.path.join(cfg.dataset_dir, "Data_Entry_2017_v2020.csv.gz"),
-                    bbox_list_path=os.path.join(cfg.dataset_dir, "BBox_List_2017.csv.gz"),
-                    transform=transforms, 
-                    data_aug=None if cfg.test_data=="nih" else data_aug, 
-                    unique_patients=False
+                imgpath=imgdir,
+                csvpath=os.path.join(cfg.dataset_dir, "Data_Entry_2017_v2020.csv.gz"),
+                bbox_list_path=os.path.join(cfg.dataset_dir, "BBox_List_2017.csv.gz"),
+                transform=transforms,
+                data_aug=None if cfg.test_data == "nih" else data_aug,
+                unique_patients=False
             )
             xrv.datasets.relabel_dataset(cfg.pathologies, nih_dataset)
             if cfg.cache_dataset:
                 os.makedirs(os.path.dirname(cache_dir), exist_ok=True)
                 torch.save((nih_dataset, imgdir), cache_dir)
-    
-    ### Load and cache CHEXPERT Dataset ###
+
+    # Load and cache CHEXPERT Dataset ###
     cx_dataset = None
     if "cx" in cfg.train_datas or "cx" in cfg.val_data or "cx" in cfg.test_data:
         imgdir = os.path.join(cfg.dataset_dir, "CheXpert-v1.0-small")
@@ -59,18 +61,18 @@ def load_data(cfg):
             cx_dataset, _ = torch.load(cache_dir)
         else:
             cx_dataset = xrv.datasets.CheX_Dataset(
-                    imgpath=imgdir,
-                    csvpath=os.path.join(cfg.dataset_dir, "CheXpert-v1.0-small/train.csv"),
-                    transform=transforms, 
-                    data_aug=None if cfg.test_data=="cx" else data_aug, 
-                    unique_patients=False
+                imgpath=imgdir,
+                csvpath=os.path.join(cfg.dataset_dir, "CheXpert-v1.0-small/train.csv"),
+                transform=transforms,
+                data_aug=None if cfg.test_data == "cx" else data_aug,
+                unique_patients=False
             )
             xrv.datasets.relabel_dataset(cfg.pathologies, cx_dataset)
             if cfg.cache_dataset:
                 os.makedirs(os.path.dirname(cache_dir), exist_ok=True)
                 torch.save((cx_dataset, imgdir), cache_dir)
 
-    ### Load and cache MIMIC_CH Dataset ###
+    # Load and cache MIMIC_CH Dataset ###
     mc_dataset = None
     if "mc" in cfg.train_datas or "mc" in cfg.val_data or "mc" in cfg.test_data:
         imgdir = os.path.join(cfg.dataset_dir, "images-224-MIMIC/files")
@@ -82,8 +84,8 @@ def load_data(cfg):
                 imgpath=imgdir,
                 csvpath=os.path.join(cfg.dataset_dir, "MIMICCXR-2.0/mimic-cxr-2.0.0-chexpert.csv.gz"),
                 metacsvpath=os.path.join(cfg.dataset_dir, "MIMICCXR-2.0/mimic-cxr-2.0.0-metadata.csv.gz"),
-                transform=transforms, 
-                data_aug=None if cfg.test_data=="mc" else data_aug, 
+                transform=transforms,
+                data_aug=None if cfg.test_data == "mc" else data_aug,
                 unique_patients=False
             )
             xrv.datasets.relabel_dataset(cfg.pathologies, mc_dataset)
@@ -91,7 +93,7 @@ def load_data(cfg):
                 os.makedirs(os.path.dirname(cache_dir), exist_ok=True)
                 torch.save((mc_dataset, imgdir), cache_dir)
 
-    ### Load and cache PADCHEST Dataset ###
+    # Load and cache PADCHEST Dataset ###
     pc_dataset = None
     if "pc" in cfg.train_datas or "pc" in cfg.val_data or "pc" in cfg.test_data:
         imgdir = os.path.join(cfg.dataset_dir, "PC/images-224")
@@ -100,11 +102,11 @@ def load_data(cfg):
             pc_dataset, _ = torch.load(cache_dir)
         else:
             pc_dataset = xrv.datasets.PC_Dataset(
-                    imgpath=imgdir,
-                    csvpath=os.path.join(cfg.dataset_dir, "PC/PADCHEST_chest_x_ray_images_labels_160K_01.02.19.csv"),
-                    transform=transforms, 
-                    data_aug=None if cfg.test_data=="pc" else data_aug, 
-                    unique_patients=False
+                imgpath=imgdir,
+                csvpath=os.path.join(cfg.dataset_dir, "PC/PADCHEST_chest_x_ray_images_labels_160K_01.02.19.csv"),
+                transform=transforms,
+                data_aug=None if cfg.test_data == "pc" else data_aug,
+                unique_patients=False
             )
             xrv.datasets.relabel_dataset(cfg.pathologies, pc_dataset)
             if cfg.cache_dataset:
@@ -119,56 +121,56 @@ def load_data(cfg):
         "mc": mc_dataset,
         "pc": pc_dataset,
     }
-    
+
     return datasets
 
 
 def create_q_model(cfg, model):
-    num_ftrs = model.fc.in_features
+    num_features = model.fc.in_features
     model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
     model_features = torch.nn.Sequential(
-                        model.quant,  
-                        model.conv1,
-                        model.bn1,
-                        model.relu,
-                        model.maxpool,
-                        model.layer1,
-                        model.layer2,
-                        model.layer3,
-                        model.layer4,
-                        model.avgpool,
-                        model.dequant,  
+        model.quant,
+        model.conv1,
+        model.bn1,
+        model.relu,
+        model.maxpool,
+        model.layer1,
+        model.layer2,
+        model.layer3,
+        model.layer4,
+        model.avgpool,
+        model.dequant,
     )
 
     new_head = torch.nn.Sequential(
-                    torch.nn.Dropout(p=cfg.dropout),
-                    torch.nn.Linear(num_ftrs, cfg.num_labels)
+        torch.nn.Dropout(p=cfg.dropout),
+        torch.nn.Linear(num_features, cfg.num_labels)
     )
 
     new_model = torch.nn.Sequential(
-                    model_features,
-                    torch.nn.Flatten(1),
-                    new_head,
+        model_features,
+        torch.nn.Flatten(1),
+        new_head,
     )
-    
+
     return new_model
 
 
 def create_model(cfg, model):
     if "resnet" in str(model.__class__):
-        num_ftrs = model.fc.in_features
+        num_features = model.fc.in_features
         model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         model.fc = torch.nn.Sequential(
-                    torch.nn.Dropout(p=cfg.dropout),
-                    torch.nn.Linear(num_ftrs, cfg.num_labels)
-        )                       
-        
+            torch.nn.Dropout(p=cfg.dropout),
+            torch.nn.Linear(num_features, cfg.num_labels)
+        )
+
     elif "densenet" in str(model.__class__):
-        num_ftrs = model.classifier.in_features
+        num_features = model.classifier.in_features
         model.features.conv0 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         model.classifier = torch.nn.Sequential(
-                    torch.nn.Dropout(p=cfg.dropout),
-                    torch.nn.Linear(num_ftrs, cfg.num_labels)
+            torch.nn.Dropout(p=cfg.dropout),
+            torch.nn.Linear(num_features, cfg.num_labels)
         )
-        
+
     return model
--- xrv_test.py
+++ xrv_test.py
@@ -1,5 +1,4 @@
 import torch.multiprocessing
-torch.multiprocessing.set_sharing_strategy('file_system')
 
 import os
 import csv
@@ -23,6 +22,8 @@ from sklearn.metrics import roc_auc_score
 import utils
 import torchxrayvision as xrv
 
+torch.multiprocessing.set_sharing_strategy('file_system')
+
 
 def tqdm(*args, **kwargs):
     if hasattr(tqdm_base, '_instances'):
@@ -34,12 +35,12 @@ def tqdm(*args, **kwargs):
 def inference(name, model, device, data_loader, criterion, limit=None):
     model.eval()
     avg_loss = []
-    task_outputs={}
-    task_targets={}
+    task_outputs = {}
+    task_targets = {}
     for task in range(data_loader.dataset[0]["lab"].shape[0]):
         task_outputs[task] = []
         task_targets[task] = []
-        
+
     with torch.inference_mode():
         t = tqdm(data_loader)
         for batch_idx, samples in enumerate(t):
@@ -50,29 +51,29 @@ def inference(name, model, device, data_loader, criterion, limit=None):
             targets = samples["lab"].to(device)
 
             outputs = model(images)
-            
+
             loss = torch.zeros(1).to(device).double()
             for task in range(targets.shape[1]):
-                task_output = outputs[:,task]
-                task_target = targets[:,task]
+                task_output = outputs[:, task]
+                task_target = targets[:, task]
                 mask = ~torch.isnan(task_target)
                 task_output = task_output[mask]
                 task_target = task_target[mask]
                 if len(task_target) > 0:
                     loss += criterion(task_output.double(), task_target.double())
-        
+
                 task_outputs[task].append(task_output.detach().cpu().numpy())
                 task_targets[task].append(task_target.detach().cpu().numpy())
             loss = loss.sum()
             avg_loss.append(loss.detach().cpu().numpy())
-            
+
         for task in range(len(task_targets)):
             task_outputs[task] = np.concatenate(task_outputs[task])
             task_targets[task] = np.concatenate(task_targets[task])
-    
+
         task_aucs = []
         for task in range(len(task_targets)):
-            if len(np.unique(task_targets[task]))> 1:
+            if len(np.unique(task_targets[task])) > 1:
                 task_auc = sklearn.metrics.roc_auc_score(task_targets[task], task_outputs[task])
                 task_aucs.append(task_auc)
             else:
@@ -85,7 +86,7 @@ def inference(name, model, device, data_loader, criterion, limit=None):
 
 def main(cfg):
     device = torch.device(cfg.device)
-    
+
     np.random.seed(cfg.seed)
     random.seed(cfg.seed)
     torch.manual_seed(cfg.seed)
@@ -96,64 +97,63 @@ def main(cfg):
         torch.backends.cudnn.benchmark = False
 
     cfg.pathologies = ["Cardiomegaly", "Effusion", "Edema", "Consolidation"]
-    
-    cfg.train_datas = [] #just placeholder needed to run the code
-    cfg.val_data = " " #just placeholder needed to run the code
-    
+
+    cfg.train_datas = []  # just placeholder needed to run the code
+    cfg.val_data = " "  # just placeholder needed to run the code
+
     datasets = utils.load_data(cfg)
     test_data = datasets[cfg.test_data]
-    
+
     test_loader = DataLoader(test_data,
-                           batch_size=cfg.batch_size,
-                           shuffle=SequentialSampler(test_data),
-                           num_workers=cfg.num_workers,
-                           pin_memory=True,
-                           drop_last=True)
-    
+                             batch_size=cfg.batch_size,
+                             shuffle=SequentialSampler(test_data),
+                             num_workers=cfg.num_workers,
+                             pin_memory=True,
+                             drop_last=True)
+
     model = xrv.models.DenseNet(weights="all")
     model = model.to(device)
 
     criterion = torch.nn.BCEWithLogitsLoss()
 
     test_auc, test_loss, task_aucs = inference(name='Inference',
-                                     model=model,
-                                     device=device,
-                                     data_loader=test_loader,
-                                     criterion=criterion,
-                                     limit=cfg.num_batches//2
-                                    )
+                                               model=model,
+                                               device=device,
+                                               data_loader=test_loader,
+                                               criterion=criterion,
+                                               limit=cfg.num_batches // 2
+                                               )
 
     task_aucs = [round(x, 4) for x in task_aucs]
     results = dict(zip(cfg.pathologies, task_aucs))
-    
-    print(f"Test loss: {test_loss:4.4f}")                                 
+
+    print(f"Test loss: {test_loss:4.4f}")
     print(json.dumps({"AUC per task": results}))
 
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description="Chest X-RAY Pathology Classification")
-    
+
     parser.add_argument("--seed", type=int, default=0, help="Seed for RNG")
     parser.add_argument("--dataset_dir", type=str, default="./data/", required=True, help="Datasets directory")
     parser.add_argument("--test_data", type=str, default=" ", required=True, help="Test dataset")
     parser.add_argument("--device", type=str, default="cpu", help="Compute architecture to use. One of ['cpu', 'cuda']")
     parser.add_argument("--cache_dataset", action="store_true", help="Whether or not to cache the dataset")
-    
-    ### Data loader
+
+    # Data loader
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
     parser.add_argument("--num_workers", type=int, default=0, help="Number of workers to run the experiment")
     parser.add_argument("--num_batches", type=int, default=430, help="Number of mini-batches to use")
 
-    ### Data Augmentation               
+    # Data Augmentation
     parser.add_argument("--data_resize", type=int, default=112, help="Size of each imgae sample to use")
     parser.add_argument("--data_aug_rot", type=int, default=45, help="Rotation degree for data augmentation")
     parser.add_argument("--data_aug_trans", type=float, default=0.15, help="Translation ratio for data augmentation")
     parser.add_argument("--data_aug_scale", type=float, default=0.15, help="Scale ratio for data augmentation")
-                        
+
     return parser
-                        
+
 
 if __name__ == "__main__":
-    cfg = get_args_parser().parse_args()
-    main(cfg)
-    
+    configuration = get_args_parser().parse_args()
+    main(configuration)
